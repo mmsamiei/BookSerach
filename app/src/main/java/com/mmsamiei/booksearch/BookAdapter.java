625acaf966606ba715d731c8e8bd302ac36f21aa
mmsamiei <mohmahsamiei@gmail.com>
2016-07-28 22:40:18 +0430

    add fetch remain menubar

--- app/src/main/java/com/mmsamiei/booksearch/BookAdapter.java
+++ app/src/main/java/com/mmsamiei/booksearch/BookAdapter.java
@@ -1,10 +1,15 @@
 package com.mmsamiei.booksearch;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -12,12 +17,34 @@ import java.util.ArrayList;
  * Created by Win2 on 7/27/2016.
  */
 public class BookAdapter extends ArrayAdapter<Book> {
+
+    private static class ViewHolder {
+        public ImageView ivCover;
+        public TextView tvTitle;
+        public TextView tvAuthor;
+    }
+
     public BookAdapter(Context context, ArrayList<Book> aBooks) {
         super(context, 0, aBooks);
     }
-    //TODO VIEW HOLDER
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final Book book = getItem(position);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_book, parent, false);
+            viewHolder.ivCover = (ImageView)convertView.findViewById(R.id.ivBookCover);
+            viewHolder.tvTitle = (TextView)convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvAuthor = (TextView)convertView.findViewById(R.id.tvAuthor);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.tvTitle.setText(book.getTitle());
+        viewHolder.tvAuthor.setText(book.getAuthor());
+        Picasso.with(getContext()).load(Uri.parse(book.getCoverUrl())).error(R.drawable.ic_nocover).into(viewHolder.ivCover);
+        return convertView;
     }
 }
--- app/src/main/java/com/mmsamiei/booksearch/BookListActivity.java
+++ app/src/main/java/com/mmsamiei/booksearch/BookListActivity.java
@@ -2,12 +2,57 @@ package com.mmsamiei.booksearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class BookListActivity extends AppCompatActivity {
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
+
+    private BookClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        lvBooks = (ListView) findViewById(R.id.lvBooks);
+        ArrayList<Book> aBooks = new ArrayList<Book>();
+        bookAdapter= new BookAdapter(this,aBooks);
+        lvBooks.setAdapter(bookAdapter);
+        fetchBooks();
+    }
+    private void fetchBooks(){
+        client = new BookClient();
+       client.getBooks("oscar",new JsonHttpResponseHandler(){
+           @Override
+           public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+               try {
+                    JSONArray docs=null;
+                   if(response!=null){
+                       docs=response.getJSONArray("docs");
+                       final ArrayList<Book> books = Book.fromJson(docs);
+                       bookAdapter.clear();
+                       for (Book book : books) {
+                           bookAdapter.add(book);
+                       }
+                       bookAdapter.notifyDataSetChanged();
+                   }
+               }
+               catch (JSONException e){
+                   e.printStackTrace();
+               }
+           }
+       });
+
     }
 }
